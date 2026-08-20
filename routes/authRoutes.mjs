5c3130060557736ba8f57fd97c5a1928c004daba
Velmurugan <velmurugan61682@gmail.com>
2026-08-20 18:08:55 +0530

    update

--- routes/authRoutes.mjs
+++ routes/authRoutes.mjs
@@ -74,6 +74,8 @@ const googleAuthHandler = (req, res, next) => {
 
 router.all('/google', googleAuthHandler);
 router.all('/google/login', googleAuthHandler);
+router.get('/callback/google', handleCallback);
 router.get('/google/callback', handleCallback);
+router.get('/callback', handleCallback);
 
 export default router;
